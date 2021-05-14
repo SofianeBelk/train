a4120fd2ea1f6c5ad718aa28a5a6469f931901ef
OUSSAMA LAARAJ <laarajoussama25@gmail.com>
2021-05-14 14:11:55 +0000

    Update data using the server and get country coordinates

---
+++ TrainLineSNCF/src/main/java/trainLine/services/CoordinateS.java
@@ -0,0 +1,59 @@
+package trainLine.services;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import trainLine.utils.BaseTools;
+
+public class CoordinateS {
+
+	
+	public static StringBuffer getCoordinates(String cities[]) throws Exception
+	{		
+		
+	    JSONObject result = new JSONObject();
+	    JSONArray jsonArray = new JSONArray();
+	    
+	    String selector="";
+	    for(int i = 0;i<cities.length;i++) {
+	    	selector +="'"+ cities[i]+"'";
+	    	if(i<cities.length-1) selector+=",";
+	    }
+	    
+	    System.out.println(selector);
+		
+	    String requete="SELECT ville_nom, ville_latitude_deg ,ville_longitude_deg FROM villes_france_free WHERE ville_nom in ("+selector+")";
+	
+		Connection connexion = BaseTools.getMySQLConnection();
+		Statement statement = connexion.createStatement();
+		ResultSet resultat = statement.executeQuery(requete);
+	
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");		
+		while(resultat.next()) {
+			result.put("villenom",resultat.getString("ville_nom"));
+			result.put("lattitude",resultat.getString("ville_latitude_deg"));
+			result.put("longitude",resultat.getString("ville_longitude_deg"));
+			sb.append(result) ;
+			sb.append(",") ;
+		}
+		System.out.println(sb.charAt(sb.length()-1));
+		if(sb.length() > 2) {
+			sb.deleteCharAt(sb.length()-1);
+		}
+		
+		sb.append("]");
+		
+		resultat.close();
+		statement.close();		
+		connexion.close();
+		
+		return sb;
+	}
+	
+
+}
---
+++ TrainLineSNCF/src/main/java/trainLine/services/UpdateData.java
@@ -0,0 +1,123 @@
+package trainLine.services;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import org.apache.log4j.BasicConfigurator;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.quartz.JobExecutionContext;
+import trainLine.utils.BaseTools;
+
+public class UpdateData implements org.quartz.Job{
+	 
+	protected static String LineTable = "tarifLine";
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) {
+    	BasicConfigurator.configure();
+    	System.out.println("Job executed at : " + LocalDateTime.now().toString());
+        System.out.println("do something...");
+
+        /**
+            1/ recup�rer tout les origins de la base et de les mettre dans une arratlist
+            2/ tu parcours les origins et pour chacun d'entre eux tu v�rifie si ce sont les meme qui se trouve dans l'api ou pas 
+            3/ si ce sont les meme pas besoin de modifer - y'a pas de nouveau distination, les prix restent inchanger
+            4/ sinon supprimer tout ce qui est en rapport avec cet origin 
+            5/ contacte l'api et insert a nouveau dans la base
+         */
+        
+        
+        try {
+            // 1 - recup�rer tout les origine de la base et de les mettre dans une arratlist
+        	System.out.println("get all origins from database and api");
+			ArrayList<String> originsDatabase = getAllOriginsFromDatabase();
+			ArrayList<String> originsApi = getAllOriginsFromDatabase(); //a modifier
+			
+			for(int i = 0 ; i<originsDatabase .size() ; i++) {
+				System.out.println("origin = "+originsDatabase.get(i));
+			}
+			
+			// 2 - [origin1, origin2...] - [origin1, origin2...]
+			for(int i = 0 ; i<originsDatabase.size() ; i++) {
+				for(int j = 0 ; j<originsApi.size() ; j++) {
+					if(originsDatabase.get(i) == null) {
+						// 3 -
+						
+					}else {
+						// 4 - sinon supprimer tout ce qui est en rapport avec cet origin
+			        	System.out.println("delete database");
+			        	Thread.sleep(100L);
+						deleteDatabase();
+						
+						// 5 - contacte l'api et insert a nouveau dans la base
+						Thread.sleep(100L);
+			        	System.out.println("re-insert every origin from api to database");
+			        	if(!originsDatabase .isEmpty()) {
+			    			TrainLine.searchTrain(originsDatabase.get(i));
+			        	}
+					}
+				}
+			}
+
+			System.out.println("end");
+			
+        
+        } catch (Exception e) {
+			e.printStackTrace();
+		}
+        
+    }
+    
+	public ArrayList<String> getAllOriginsFromDatabase() throws SQLException {
+		ArrayList<String> origins = new ArrayList<String>() ; 
+		String requete="select origin from "+LineTable+" ";    
+		
+		Connection connexion = BaseTools.getMySQLConnection();
+		Statement statement = connexion.createStatement();		
+		ResultSet resultat = statement.executeQuery(requete);
+		
+		while(resultat.next()) {			    
+			origins.add(resultat.getString("origin"));
+		}
+		
+		statement.close();
+		connexion.close();
+
+		return origins;	
+	}
+	
+	//retourner les origin (celle de la base) depuis l'api
+	public void getOriginsFromApi(JSONObject j, ArrayList<String> origins) throws Exception{
+		JSONArray je = j.getJSONArray("records");
+		ArrayList<String> os = new ArrayList<String>();
+		
+		for(int i=0 ;i<je.length();i++) {
+			j= (JSONObject) je.get(i);
+			JSONObject train = new JSONObject(j.get("fields").toString());
+	
+			String origin = train.getString("origine");
+			os.add(origin) ; 
+		}
+		
+		//TODO
+     }
+	
+	public void deleteDatabase() throws SQLException{
+		String requete="delete from "+LineTable+" ";
+		Connection connexion = BaseTools.getMySQLConnection();
+		Statement statement = connexion.createStatement();
+	
+		statement.executeUpdate(requete);
+		statement.close();
+		connexion.close();
+	}
+	
+	
+	
+	
+
+}
